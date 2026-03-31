cb9d4b76855a64b45da3ce5097f8676f036fc6cc
CaptainIgelo <109104565+CaptainIgelo@users.noreply.github.com>
2026-03-31 12:48:07 +0000

    feat: add filtering, search and ordering to TaskViewSet

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets 
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -7,3 +8,7 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by('-created_at')
     serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['status']
+    search_fields = ['title', 'description']
+    ordering_fields = ['created_at', 'status']
